1cc14e91fa8350d1aed4f817f495c1d7c0a666bc
yaiza <yaizafv2004@gmail.com>
2026-09-10 13:07:29 +0200

    Create E5.sql

---
+++ PLSQL/SQL4/E5.sql
@@ -0,0 +1,19 @@
+create or replace PROCEDURE SQL4E5 AS 
+vName Client.client_name%TYPE;
+cursor c1 is SELECT b1.branch_name
+FROM Branch b1
+WHERE NOT EXISTS (
+    SELECT *
+    FROM Branch b2
+    WHERE b2.branch_city = 'Brooklyn'
+      AND b1.assets <= b2.assets
+);
+BEGIN
+  OPEN c1;
+    FETCH c1 into vName;
+    WHILE (c1%FOUND) loop
+       DBMS_OUTPUT.put_line(vName);
+       FETCH c1 into vName;
+    END LOOP;
+    CLOSE c1;
+END SQL4E5;
